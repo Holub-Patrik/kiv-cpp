99e983c2addf99cedebc8943ecd4039aa0e993d4
Holub Patrik <23bulohp@gmail.com>
2024-11-26 01:11:33 +0100

    Made it into a lambda mess, but technically I have par_unseq implemented

--- sp2/main.cpp
+++ sp2/main.cpp
@@ -1,7 +1,10 @@
+#include <algorithm>
 #include <array>
 #include <cstdint>
+#include <execution>
 #include <limits>
 #include <print>
+#include <ranges>
 
 template <typename T>
 static consteval T template_max(const T& lhs, const T& rhs) {
@@ -12,86 +15,95 @@ static consteval T template_max(const T& lhs, const T& rhs) {
   }
 }
 
-template <uint64_t max_bytes> class BigNumber {
+template <uint64_t size> class BigNumber {
 private:
   // this means I get 4 bytes for each position
   // all operations I need to implement can be done per parts
   // also dividing by 10 can be done per parts (for converting into decimal)
+  const uint64_t bytes = size;
+  std::array<uint32_t, size / 4> repr;
 
 public:
-  std::array<uint32_t, max_bytes / 4> repr;
-  const uint64_t size = max_bytes;
   // default without constructor
-  BigNumber() : repr(std::array<uint32_t, max_bytes / 4>{}) {
+  BigNumber() : repr(std::array<uint32_t, size / 4>{}) {
     // std::println("Array length: {}", repr.size());
   }
-  // initialize given an uniternal represantation to be used
-  BigNumber(std::array<uint32_t, max_bytes / 4> init_arr) : repr(init_arr) {
+  // initialize given an internal represantation to be used
+  BigNumber(std::array<uint32_t, size / 4> init_arr) : repr(init_arr) {
     // std::println("Array length: {}", repr.size());
   }
   // initialize using a number
   // allow only for initializing using uint32_t
-  BigNumber(uint32_t num) : repr(std::array<uint32_t, max_bytes / 4>{}) {
+  BigNumber(uint32_t num) : repr(std::array<uint32_t, size / 4>{}) {
     repr[0] = num;
     // std::println("Array length: {}", repr.size());
   }
 
   ~BigNumber() {}
 
-  template <uint64_t other_size>
-  BigNumber<template_max(other_size, max_bytes)>
-  operator+(BigNumber<other_size> const& other) {
-    const uint64_t resulting_size = template_max(other_size, max_bytes);
-
-    std::array<uint32_t, resulting_size / 4> res_arr{};
-    int elem_index = 0;
-    uint64_t carry = 0;
-
-    auto elem_iter = res_arr.begin();
-    auto other_iter = other.repr.begin();
-    auto this_iter = repr.begin();
-
-    while (elem_iter != res_arr.end()) {
-      uint64_t other_num;
-      if (other_iter != other.repr.end()) {
-        other_num = *other_iter;
-      } else {
-        other_num = 0;
-      }
-
-      uint64_t this_num;
-      if (this_iter != repr.end()) {
-        this_num = *this_iter;
-      } else {
-        this_num = 0;
-      }
-
-      uint64_t temp = this_num + other_num + carry;
-
-      if (temp > std::numeric_limits<uint32_t>::max()) {
-        carry = temp >> 32;
-        *elem_iter = static_cast<uint32_t>(temp);
-      } else {
-        *elem_iter = static_cast<uint32_t>(temp);
-        carry = 0;
-      }
-
-      elem_iter++;
-      other_iter++;
-      this_iter++;
-    }
-
-    return BigNumber<resulting_size>(res_arr);
+  constexpr const uint64_t max_bytes() const noexcept { return bytes; }
+  constexpr const std::array<uint32_t, size / 4>& get_repr() const noexcept {
+    return repr;
   }
 };
+
+template <uint64_t lhs_size, uint64_t rhs_size>
+BigNumber<template_max(lhs_size, rhs_size)>
+operator+(BigNumber<lhs_size> const& lhs, BigNumber<rhs_size> const& rhs) {
+  const uint64_t resulting_size = template_max(lhs_size, rhs_size);
+  const auto& rhs_arr = rhs.get_repr();
+  const auto& lhs_arr = lhs.get_repr();
+
+  std::array<uint32_t, resulting_size / 4> res_arr{};
+  std::array<uint32_t, resulting_size / 4> carry_arr{};
+  auto index_gen = std::views::iota(0);
+  auto index = std::views::counted(index_gen.begin(), resulting_size / 4);
+
+  // copy the shorter array into the res_arr
+  // the shorter is copied so I can work easily with same length one
+  if (lhs_size > rhs_size) {
+    std::copy(rhs_arr.begin(), rhs_arr.end(), res_arr.begin());
+    auto test = *index.begin();
+    std::for_each(std::execution::par_unseq, index.begin(), index.end(),
+                  [&carry_arr, &res_arr, lhs_arr](auto index) {
+                    const uint64_t temp =
+                        static_cast<uint64_t>(res_arr[index]) +
+                        static_cast<uint64_t>(lhs_arr[index]);
+                    res_arr[index] = static_cast<uint32_t>(temp);
+                    carry_arr[index] = temp >> 32;
+                  });
+
+  } else {
+    std::copy(lhs_arr.begin(), lhs_arr.end(), res_arr.begin());
+
+    std::for_each(std::execution::par_unseq, index.begin(), index.end(),
+                  [&carry_arr, &res_arr, rhs_arr](auto index) {
+                    const uint64_t temp =
+                        static_cast<uint64_t>(res_arr[index]) +
+                        static_cast<uint64_t>(rhs_arr[index]);
+                    res_arr[index] = static_cast<uint32_t>(temp);
+                    carry_arr[index] = temp >> 32;
+                  });
+  }
+
+  std::for_each(std::execution::par_unseq, index.begin(), index.end(),
+                [&carry_arr, &res_arr](auto index) {
+                  if (index == 0) {
+                    return;
+                  }
+                  res_arr[index] += carry_arr[index - 1];
+                });
+
+  return BigNumber<resulting_size>(res_arr);
+}
 
 int main(int argc, char* argv[]) {
   BigNumber<12> bg1{std::numeric_limits<uint32_t>::max()};
   BigNumber<16> bg2{std::numeric_limits<uint32_t>::max()};
 
-  auto bg3 = bg1 + bg2 + bg2 + bg2;
-  std::println("\n{}\n", bg3.size);
-  for (auto& elem : bg3.repr) {
+  auto bg3 = bg1 + bg2;
+  std::println("\n{}\n", bg3.max_bytes());
+  for (auto& elem : bg3.get_repr()) {
     std::println("{}", elem);
   }
   return 0;
