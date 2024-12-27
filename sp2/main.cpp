45a79832bdd71a3ae7febdfe2133f6bf6b8224f6
Holub Patrik <23bulohp@gmail.com>
2024-12-27 17:36:33 +0100

    Decadic representation, figure out how to increase size if adition overflows

--- sp2/main.cpp
+++ sp2/main.cpp
@@ -6,10 +6,12 @@
 #include <exception>
 #include <execution>
 #include <format>
-#include <limits>
-#include <numbers>
+#include <iterator>
 #include <print>
 #include <ranges>
+#include <sstream>
+#include <string>
+#include <utility>
 
 // the complete insanity done in operator+() will be done for muptiplication too
 // the adition of the carry array will have to be done twice
@@ -23,8 +25,7 @@ static consteval T template_max(const T& lhs, const T& rhs) {
   } else {
     return rhs;
   }
-} // namespace BigNumbertemplate<typenameT>staticconstevalT
-  // template_max(constT&lhs,constT&rhs) template_max(constT&lhs,constT&rhs)
+}
 
 template <typename T>
 concept SizeParam = std::convertible_to<T, uint64_t> && requires(T t) {
@@ -33,20 +34,6 @@ concept SizeParam = std::convertible_to<T, uint64_t> && requires(T t) {
   { t % t } -> std::convertible_to<T>;
   { t / t } -> std::convertible_to<T>;
 };
-
-static consteval uint64_t size_ceil(SizeParam auto val, SizeParam auto div) {
-  const uint64_t temp = div - val % div;
-  return (val + temp) / div;
-}
-
-template <typename T> static consteval uint64_t const_ceil(const T& num) {
-  uint64_t temp = static_cast<uint64_t>(num);
-  if (temp < num) {
-    return temp + 1;
-  } else {
-    return temp;
-  }
-}
 
 class BigNumberException : std::exception {
 public:
@@ -61,174 +48,112 @@ private:
   std::string _msg;
 };
 
-template <uint64_t size> class BigInt {
+template <size_t size> class BigInt {
 private:
-  // this means I get 4 bytes for each position
-  // all operations I need to implement can be done per parts
-  // also dividing by 10 can be done per parts (for converting into decimal)
-  const uint64_t bytes = size;
-  std::array<uint32_t, size_ceil(size, 4)> repr;
+  bool negative = false;
+  const size_t order = size;
+  std::array<short, size> repr{};
 
 public:
-  // default without constructor
-  BigInt() : repr(std::array<uint32_t, size_ceil(size, 4)>{}) {
-    // std::println("Array length: {}", repr.size());
-  }
-  // initialize given an internal represantation to be used
-  BigInt(std::array<uint32_t, size_ceil(size, 4)> init_arr) : repr(init_arr) {
-    // std::println("Array length: {}", repr.size());
-  }
-  // initialize using a number
-  // allow only for initializing using uint32_t
-  BigInt(uint32_t num) : repr(std::array<uint32_t, size_ceil(size, 4)>{}) {
-    repr[0] = num;
-    // std::println("Array length: {}", repr.size());
-  }
-
+  // default constructor
+  BigInt() {}
   ~BigInt() {}
 
-  constexpr const uint64_t max_bytes() const noexcept { return bytes; };
-  constexpr const std::array<uint32_t, size_ceil(size, 4)>&
-  get_repr() const noexcept {
-    return repr;
+  // initialize using a number
+  // allow only for initializing using
+  BigInt(long num) : repr(std::array<short, size>{}) {
+    int i = 0;
+    while (num >= 10) {
+      repr[i] = num % 10;
+      num /= 10;
+      ++i;
+    }
+
+    repr[i] = num;
   }
 
-  constexpr bool is_negative() {
-    auto last_elem = repr.back();
-    return (last_elem >> 31) & 1;
+  BigInt(std::array<short, size>&& repr) : repr(repr) {}
+
+  // move constructor
+  BigInt(BigInt<size>&& other) : repr(other.repr), negative(other.negative) {}
+
+  // copy constructor
+  BigInt(BigInt<size>& other_num) {
+    negative = other_num.negative;
+    repr = other_num.repr;
   }
 
-  BigInt<size> get_complement() const {
-    auto complement_repr = repr;
-    std::for_each(std::execution::par_unseq, complement_repr.begin(),
-                  complement_repr.end(), [](auto& elem) { elem = !elem; });
+  // copy assignment
+  BigInt& operator=(const BigInt<size>& other) {
+    if (this == &other)
+      return *this;
 
-    BigInt<size> complement{complement_repr};
-    complement += BigInt<size>{1};
-    return complement;
+    BigInt<size> temp(other);
+    std::swap(repr, temp.repr);
+    negative = temp.negative;
+
+    return *this;
   }
+
+  // move assignment
+  BigInt& operator=(BigInt<size>&& other) noexcept {
+    BigInt<size> temp(std::move(other));
+    std::swap(repr, temp.repr);
+    negative = temp.negative;
+    return *this;
+  }
+
+  constexpr const uint64_t num_order() const noexcept { return order; };
+  const std::array<short, size>& get_repr() const noexcept { return repr; }
+  constexpr bool is_negative() const { return negative; }
 
   template <uint64_t other_size>
-  BigInt<size> operator+=(BigInt<other_size> rhs);
+  BigInt<template_max(size, other_size)> operator+=(BigInt<other_size> rhs);
 
-  // template <uint64_t other_size>
-  // BigInt<size> operator+=(BigInt<other_size> rhs) {
-  //   if (other_size > size) {
-  //     throw BigNumberException("Cannot do this!");
-  //   }
-  //
-  //   const auto& rhs_arr = rhs.get_repr();
-  //
-  //   auto& res_arr = repr;
-  //   const auto& work_arr = rhs.get_repr();
-  //   std::array<uint32_t, size> carry_arr{};
-  //   auto index_gen = std::views::iota(0);
-  //   auto index = std::views::counted(index_gen.begin(), size);
-  //
-  //   std::for_each(std::execution::par_unseq, index.begin(), index.end(),
-  //                 [&carry_arr, &res_arr, work_arr](auto index) {
-  //                   const uint64_t temp =
-  //                       static_cast<uint64_t>(res_arr[index]) +
-  //                       static_cast<uint64_t>(work_arr[index]);
-  //                   res_arr[index] = static_cast<uint32_t>(temp);
-  //                   carry_arr[index] = temp >> 32;
-  //                 });
-  //
-  //   std::for_each(std::execution::par_unseq, index.begin(), index.end(),
-  //                 [&carry_arr, &res_arr](auto index) {
-  //                   if (index == 0) {
-  //                     return;
-  //                   }
-  //                   res_arr[index] += carry_arr[index - 1];
-  //                 });
-  //
-  //   return *this;
-  // }
+  const std::string to_string() const {
+    std::ostringstream s;
+    std::for_each(repr.rbegin(), repr.rend(), [&s](auto elem) { s << elem; });
 
-  operator std::string() const {
-    // division by ten seems to always shift the number by 3 bits
-    // this I can pick the 3 lower bits of the next byte, shift them to top
-    // and add them to the current number
-    // chars count eq:
-    // n * ln(P) / ln(Q)t
-    // Q = dest chars (dec)
-    // P = source chars (bin)
-    // I will only be converting to decadic so
-    //  ln(P) = ln(2)
-    //  ln(Q) = ln(10)
-    const uint64_t chars_count =
-        const_ceil(size * 8 * std::numbers::ln2 / std::numbers::ln10);
-    std::array<char, chars_count> chars{};
-
-    size_t index = 0;
-    while (repr[0] > 0) {
-      chars[index] = repr[0] % 10;
-      repr[0] /= 10;
-      for (size_t shift_index = 1; shift_index < repr.size(); ++shift_index) {
-        uint64_t lower_three = repr[shift_index] & 7; // take out lower 3 bits
-        lower_three <<= 29; // shift them to the highest 3 bits
-        repr[shift_index - 1] += lower_three; // add them to the elem before
-        repr[shift_index] >>= 3; // shift by 3 bits since they were moved lower
-      }
-
-      ++index;
-    }
+    return std::string{s.str()};
   }
 };
 
-template <uint64_t size>
-template <uint64_t other_size>
-BigInt<size> BigInt<size>::operator+=(BigInt<other_size> rhs) {}
+template <size_t size>
+template <size_t other_size>
+BigInt<template_max(size, other_size)>
+BigInt<size>::operator+=(BigInt<other_size> rhs) {
+  return *this + rhs;
+}
 
-template <uint64_t lhs_size, uint64_t rhs_size>
+template <size_t lhs_size, size_t rhs_size>
 BigInt<template_max(lhs_size, rhs_size)>
-operator+(BigInt<lhs_size> const& lhs, BigInt<rhs_size> const& rhs) {
-  constexpr const uint64_t resulting_size = template_max(lhs_size, rhs_size);
+operator+(const BigInt<lhs_size>& lhs, const BigInt<rhs_size>& rhs) {
+  constexpr const size_t resulting_size = template_max(lhs_size, rhs_size);
   const auto& rhs_arr = rhs.get_repr();
   const auto& lhs_arr = lhs.get_repr();
 
-  std::array<uint32_t, size_ceil(resulting_size, 4)> res_arr{};
-  std::array<uint32_t, size_ceil(resulting_size, 4)> work_arr{};
-  std::array<uint32_t, size_ceil(resulting_size, 4)> carry_arr{};
+  std::array<short, resulting_size> res_arr;
+  std::array<short, resulting_size> carry_arr;
   auto index_gen = std::views::iota(0);
-  auto index = std::views::counted(index_gen.begin(), resulting_size / 4);
-
-  // copy the shorter array into the res_arr
-  // copy the longer one to work array to be worked on
-  // I would love to just somehow alias the work array and not copy it, but I do
-  // not know how
-  if (lhs_size > rhs_size) {
-    std::copy(rhs_arr.begin(), rhs_arr.end(), res_arr.begin());
-    std::copy(lhs_arr.begin(), lhs_arr.end(), work_arr.begin());
-  } else {
-    std::copy(lhs_arr.begin(), lhs_arr.end(), res_arr.begin());
-    std::copy(rhs_arr.begin(), rhs_arr.end(), work_arr.begin());
-  }
+  auto index = std::views::counted(index_gen.begin(), resulting_size);
 
   std::for_each(std::execution::par_unseq, index.begin(), index.end(),
-                [&carry_arr, &res_arr, work_arr](auto index) {
-                  const uint64_t temp = static_cast<uint64_t>(res_arr[index]) +
-                                        static_cast<uint64_t>(work_arr[index]);
-                  res_arr[index] = static_cast<uint32_t>(temp);
-                  carry_arr[index] = temp >> 32;
+                [&carry_arr, &res_arr, &rhs_arr, &lhs_arr](auto index) {
+                  const short lhs_num =
+                      index == lhs_arr.size() ? 0 : lhs_arr[index];
+                  const short rhs_num =
+                      index == rhs_arr.size() ? 0 : rhs_arr[index];
+                  const short temp = lhs_num + rhs_num;
+                  res_arr[index] = temp % 10;
+                  carry_arr[index] = temp / 10;
                 });
 
-  std::for_each(std::execution::par_unseq, index.begin(), index.end(),
+  std::for_each(std::execution::par_unseq, index.begin() + 1, index.end(),
                 [&carry_arr, &res_arr](auto index) {
-                  if (index == 0) {
-                    return;
-                  }
                   res_arr[index] += carry_arr[index - 1];
                 });
 
-  return BigInt<resulting_size>(res_arr);
-}
-
-template <uint64_t lhs_size, uint64_t rhs_size>
-BigInt<template_max(lhs_size, rhs_size)>
-operator-(BigInt<lhs_size> const& rhs, BigInt<rhs_size> const& lhs) {
-  lhs.get_complement();
-  return rhs + lhs;
+  return BigInt<resulting_size>{std::move(res_arr)};
 }
 
 template <uint64_t lhs_size, uint64_t rhs_size>
@@ -267,16 +192,10 @@ auto operator<=>(BigInt<lhs_size> const& rhs, BigInt<rhs_size> const& lhs) {
 }
 
 int main(int argc, char* argv[]) {
-  BigInt<40> bg1{std::numeric_limits<uint32_t>::max()};
-  BigInt<50> bg2{std::numeric_limits<uint32_t>::max()};
-  BigInt<90> test{std::numeric_limits<uint32_t>::max()};
+  BigInt<4> bg1{6000};
+  BigInt<5> bg2{6000};
 
-  auto bg3 = bg1 - bg2;
-  auto bg4 = bg1 + bg2;
-  bg4 += bg2;
-  std::println("{}\n", bg1 < bg3);
-  for (auto& elem : bg3.get_repr()) {
-    std::println("{}", elem);
-  }
+  auto bg3 = bg1 + bg2;
+  std::println("{}", bg3.to_string());
   return 0;
 }
